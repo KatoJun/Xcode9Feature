e402605d95eff259b1257fe03079d4784e22b93e
cm-kato-jun <kato.jun@classmethod.jp>
2017-09-04 11:29:29 +0900

    CodeFoldingを追加

---
+++ Xcode9Feature/CodeFolding.swift
@@ -0,0 +1,26 @@
+//
+//  CodeFolding.swift
+//  Xcode9Feature
+//
+//  Created by kato.jun on 2017/09/04.
+//  Copyright © 2017年 Classmethod, Inc. All rights reserved.
+//
+
+import Foundation
+
+/// クラスの説明
+class MyClass {
+
+    /// プロパティの説明
+    var property1: String?
+
+    /// メソッドの説明をここに書く
+    ///
+    /// - Parameters:
+    ///   - param1: パラメーター1の説明
+    ///   - param2: パラメーター2の説明
+    /// - Returns: 戻り値の説明
+    func func1(param1: Int, param2: String) -> String {
+        return ""
+    }
+}
